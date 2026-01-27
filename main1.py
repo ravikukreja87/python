c99f63cce9fb871ea94a5214c5a3f4f2f79bdc2a
ravikukreja87 <ravi8737@gmail.com>
2026-01-27 20:42:35 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,4 @@
+def weather(temp):
+    print("Today's Tempreture is",temp)
+temp=int(input("Enter Today's Tempreture : "))
+weather(temp)
---
+++ main2.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+def subract(z,a):
+    return z-a
+def multiply(b,c):
+    return b*c
+def divide(d,g):
+    return d/g
+
+para1=int(input("Enter the first value : "))
+para2=int(input("Enter the second value : "))
+
+print(add(para1,para2))
+print(subract(para1,para2))
+print(multiply(para1,para2))
+print(divide(para1,para2))
