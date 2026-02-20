caa7c098757f3a79aff98bfdc9de2f3b98f5a120
ravikukreja87 <ravi8737@gmail.com>
2026-02-20 19:07:44 +0530

    Add files via upload

---
+++ tuplex.py
@@ -0,0 +1,15 @@
+tuplex=(2,6,4,5,3,3,4,5,3,4,5,5,5,4)
+print(tuplex)
+
+tuplex=(False, 4.23)
+print(tuplex)
+
+tuplex=tuplex + (9,)
+print(tuplex)
+
+tuplint=(2,3,4,0,5,6,7,8,9,6,4)
+print(tuplint.count(5))
+
+print(tuplint[3:5])
+
+print(tuplint[:6])
