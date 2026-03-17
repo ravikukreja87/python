6eb1bc5372680cc6e586ded7eb1f9a8794504ac2
ravikukreja87 <ravi8737@gmail.com>
2026-03-17 20:42:53 +0530

    Add files via upload

---
+++ duplicate.py
@@ -0,0 +1,8 @@
+
+a={"name":"abc","grade":12,"age":16}
+
+print(a)
+
+a["name"]="xyz"
+
+print(a)
---
+++ frequency.py
@@ -0,0 +1,9 @@
+a={"name" : "efg" , "age" : 12 , "grade" : 12 }
+k=12
+result=0
+
+for i in a :
+    if a [i] == k:
+        result = result + 1
+        
+print (result)
